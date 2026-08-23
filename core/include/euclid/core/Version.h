b4022657ca6ace436e8fdd45a512bb75b0e79be0
jensv <jens.vogt@opitz-consulting.com>
2026-08-24 00:05:04 +0200

    fix: windows port

--- core/include/euclid/core/Version.h
+++ core/include/euclid/core/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.0.0"
+#define APP_VERSION "1.0.15"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 0
-#define APP_VERSION_PATCH 0
+#define APP_VERSION_PATCH 15
